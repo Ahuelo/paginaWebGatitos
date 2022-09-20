e2977ad58f5266a13d8e47eb469a6c533e999931
Ivan Montenegro <nelson.moncar@outlook.com>
2022-09-20 16:17:48 -0500

    conexión mediante async

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,26 @@
 const API = 'https://api.thedogapi.com/v1/images/search';
 const btnRecharge = document.querySelector('#btnRandom');
 
-fetch(API)
+/* fetch(API)
 .then(res => res.json())
 .then(data => {
     const img = document.querySelector('#img');
-    /* img.setAttribute.add('src'); */
     img.src = data[0].url;
-});
+}); */
+
+async function urlDog () {
+    const res = await fetch (API);
+    const data = await res.json();
+    const img = document.querySelector('#img');
+    img.src = data[0].url;
+}
+
+urlDog();
 
 btnRecharge.addEventListener('click', btnchange);
 
 function btnchange () {
     location.reload();
 }
+
+
